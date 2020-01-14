fae084b21e5764206d0c11decdf795715229b659
shaanzie <ishaanlagwankar@gmail.com>
2020-01-14 07:15:53 +0530

    Working with frames

---
+++ 2/ajax - frames.php
@@ -0,0 +1,21 @@
+<?php
+
+    extract($_GET);
+
+    $res = "";
+
+    if($srn == "SRN1"){
+        $res = "name:abc; gpa:8.0";
+    }
+    
+    if($srn == "SRN2"){
+        $res = "name:def; gpa:5.0";
+    }
+
+    // echo $res;
+    echo "<html>";
+    echo "<script>";
+    echo "parent.obj.updateDetails('$res');";    
+    echo "</script>";
+    echo "</html>";
+?>
