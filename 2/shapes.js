3cd21174cd788b3414fe33760bf578579b1f4fe0
shaanzie <ishaanlagwankar@gmail.com>
2020-01-14 06:00:33 +0530

    JS objects

--- 2/shapes.js
+++ 2/shapes.js
@@ -15,13 +15,14 @@ var rect = new Rectangle(3, 4);
 rect.toString();
 
 // To show inheritance
+// function Square(size) {
+//     this.length = size;
+//     this.breadth = size;
+// }
 
-function Square(size) {
-    this.length = size;
-    this.breadth = size;
-}
-
+// Square.prototype has to be instantiated as a inheritance of rectangle object
 Square.prototype = new Rectangle();
+
 Square.prototype.constructor = Square;
 
 Square.prototype.toString = function() {
@@ -33,3 +34,12 @@ console.log(sq.area());
 console.log(sq.toString());
 
 // We don't assign Square's prototype to the Rectangle's prototype to avoid the Rectangle accessing the Square.
+
+// This does not work as normal inheritance methods, so the super keyword is used, and this is called construcotr stealing
+function Square(size) {
+
+    // Call rectangle constructor by using call and apply
+    Rectangle.apply(this, [size, size]);
+    // Another way of doing this
+    // Rectangle.call(this, size, size);
+}
