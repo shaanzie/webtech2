f58f9aaab2070669cac181e0952ee043f74c73e9
shaanzie <ishaanlagwankar@gmail.com>
2020-01-14 05:35:11 +0530

    shapes

--- 2/shapes.js
+++ 2/shapes.js
@@ -24,7 +24,12 @@ function Square(size) {
 Square.prototype = new Rectangle();
 Square.prototype.constructor = Square;
 
+Square.prototype.toString = function() {
+    return "Square: " + this.length + "units";
+}
+
 var sq = new Square(5);
 console.log(sq.area());
+console.log(sq.toString());
 
 // We don't assign Square's prototype to the Rectangle's prototype to avoid the Rectangle accessing the Square.
