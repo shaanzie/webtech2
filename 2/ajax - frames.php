9567b5a34c2b84923257929109f6bf7a25700098
shaanzie <ishaanlagwankar@gmail.com>
2020-01-16 05:31:42 +0530

    Frames sent

--- 2/ajax - frames.php
+++ 2/ajax - frames.php
@@ -5,11 +5,11 @@
     $res = "";
 
     if($srn == "SRN1"){
-        $res = "name:abc; gpa:8.0";
+        $res = "name:abc;gpa:8.0";
     }
     
     if($srn == "SRN2"){
-        $res = "name:def; gpa:5.0";
+        $res = "name:def;gpa:5.0";
     }
 
     // echo $res;
