360acf2a7bb01f3aab7b8bbb0f1368e3183a533e
shaanzie <ishaanlagwankar@gmail.com>
2020-01-22 12:44:57 +0530

    XHR requests

--- 4/getsport.php
+++ 4/getsport.php
@@ -1,7 +1,7 @@
 <?php
   // extract($_GET);
   extract($_POST);
-
+sleep(5);
   if($ath == "VK47"){
     $res = array("sport"=>"football","country"=>"portugal");
   }
--- 5/video.php
+++ 5/video.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-type:video/mp4");
-
+sleep(5);
 $file = fopen("s.mp4","r");
 $data = fread($file, filesize("s.mp4"));
 echo $data;
